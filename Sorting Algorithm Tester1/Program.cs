00f0802af4d240926194a16d2058d69bab614dbd
jiikeissi <jiikeissi@gmail.com>
2013-11-29 18:05:34 +0200

    Commit #1

---
+++ Sorting Algorithm Tester1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting_Algorithm_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingTestData testData = new SortingTestData();
+            testData.InitRandom(10, 10);
+            testData.testOutput();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Sorting Algorithm Tester1/SortingTestData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sorting_Algorithm_Tester
+{
+    class SortingTestData
+    {
+        private int[] testData;                 //Integer array for testing data
+        private int N;                          //Size of the testing data table
+        private int maxNumber;                  //Inclusive upper bound for test data integers
+        private Random rndGen = new Random();  //Random number generator
+
+        public 
+
+        /// <summary>
+        /// Init the testData array to random numbers in a random order
+        /// </summary>
+        /// <param name="maxNumber">Inclusive upper bound for values of integers to be included</param>
+        /// <param name="N">Size of the array</param>
+        public void InitRandom(int maxNumber, int N)
+        {
+            this.N = N;
+            this.maxNumber = maxNumber;
+
+            testData = new int[N];
+            for (int i = 0; i < N; i++)
+                testData[i] = rndGen.Next(0, maxNumber+1);
+        }
+
+        /// <summary>
+        /// Init the testData array to random numbers in an ascending order
+        /// </summary>
+        /// <param name="maxNumber">Inclusive upper bound for values of integers to be included</param>
+        /// <param name="N">Size of the array</param>
+        public void initAscending(int maxNumber, int N)
+        {
+            this.N = N;
+            this.maxNumber = maxNumber;
+        }
+
+        /// <summary>
+        /// Init the testData array to random numbers in a descending order
+        /// </summary>
+        /// <param name="maxNumber">Inclusive upper bound for values of integers to be included</param>
+        /// <param name="N">Size of the array</param>
+        public void initDescending(int maxNumber, int N)
+        {
+            this.N = N;
+            this.maxNumber = maxNumber;
+        }
+
+        public void testOutput()
+        {
+            foreach (int x in testData)
+                Console.WriteLine(x);
+        }
+    }
+}
